d96442c348940a03e9561d081c240492f9efb20e
Administrator@PQ-20240319EKAD <1016635749@qq.com>
2025-03-20 15:48:42 +0800

    新增内容

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 def main():
     st.title("我的 Streamlit 应用")
     st.write("欢迎使用 Streamlit!")
+    st.header("这是一个标题")
     # 在这里添加更多的 Streamlit 代码
  
 if __name__ == "__main__":
