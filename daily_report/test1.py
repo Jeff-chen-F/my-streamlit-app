84bf33a71423baa82afec7e81a5d945a701bcae3
Administrator@PQ-20240319EKAD <1016635749@qq.com>
2025-04-26 11:34:26 +0800

    1

--- daily_report/test1.py
+++ daily_report/test1.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 list1={"A":[1,2,3],"B":[4,5,6]}
 df1=pd.DataFrame(list1)
-df1.to_html("daily_report.html")
+df=df1.to_html("daily_report.html")
+
+with open(rf"D:\rp_test\rp.github.io") as f:
+    f.write(df)
